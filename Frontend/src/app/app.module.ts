f0f032c97a393cccf6901fda8e3126d405635c62
MadushanDinusha <djabc.pwc@gmail.com>
2020-06-25 09:22:47 +0530

    Update: login page updated

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
