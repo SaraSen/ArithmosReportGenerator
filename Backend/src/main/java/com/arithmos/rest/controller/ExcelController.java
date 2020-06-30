165295193b8fed60c91fec517c8a5ef276dc9063
MadushanDinusha <djabc.pwc@gmail.com>
2020-06-30 11:07:43 +0530

    Update: date range sends to the backend

--- Backend/src/main/java/com/arithmos/rest/controller/ExcelController.java
+++ Backend/src/main/java/com/arithmos/rest/controller/ExcelController.java
@@ -2,14 +2,15 @@ package com.arithmos.rest.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.arithmos.rest.service.ExcelOutputService;
 
 @RestController
@@ -20,13 +21,14 @@ public class ExcelController {
 	@Autowired
 	private ExcelOutputService excelOutputService;
 
-	@GetMapping(value = "tasks")
-	public ResponseEntity<InputStreamResource> excelReport() throws IOException {
-		ByteArrayInputStream in = excelOutputService.contactListToExcelFile();
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "attachment; filename=report.xlsx");
-
-		return ResponseEntity.ok().headers(headers).body(new InputStreamResource(in));
+	@PostMapping(value = "tasks")
+	public ResponseEntity<?> excelReport(@RequestBody Map<String,Date> dateRange) throws IOException {
+		System.out.println(dateRange);
+//		ByteArrayInputStream in = excelOutputService.contactListToExcelFile();
+//		HttpHeaders headers = new HttpHeaders();
+//		headers.add("Content-Disposition", "attachment; filename=report.xlsx");
+	return new ResponseEntity<>(HttpStatus.OK);
+		//return ResponseEntity.ok().headers(headers).body(new InputStreamResource(in));
 	}
 
 }
